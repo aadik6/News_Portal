f7450d43e9bb7c946a1ed78b3a4057738308b4bc
aadik6 <aadarshkumarman@gmail.com>
2024-06-22 07:20:47 +0545

    validate

--- src/pages/addNews/addNews.jsx
+++ src/pages/addNews/addNews.jsx
@@ -38,16 +38,24 @@ function AddNews() {
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
-    setFile(selectedFile);
+    const validTypes = ["image/jpeg", "image/png"];
+    if (selectedFile && validTypes.includes(selectedFile.type)) {
+      setFile(selectedFile);
+    } else {
+      toast.error("Only JPEG and PNG files are allowed!");
+      inputFiledRef.current.value = "";
+    }
   };
 
   const handlePostNews = async () => {
-    // const alphanumericWithLetterRegex = /^(?=.*[a-zA-Z])[a-zA-Z\s]+$/;
-    // const descriptionRegex = /^(?=.*[a-zA-Z])[a-zA-Z0-9\s]+$/;
+    const isHeadingOnlyNumbers = /^\d+$/.test(formData.heading);
 
-    // Check if heading or description is empty
     if (!formData.heading) {
       toast.error("Heading cannot be empty!");
+      return;
+    }
+    if (isHeadingOnlyNumbers) {
+      toast.error("Heading cannot be only numbers!");
       return;
     }
     if (!formData.description) {
@@ -59,43 +67,20 @@ function AddNews() {
       return;
     }
 
-    // // Check if heading does not contain numbers and contains at least one letter
-    // if (!alphanumericWithLetterRegex.test(formData.heading)) {
-    //   toast.error(
-    //     "Heading must not contain numbers and must contain at least one letter"
-    //   );
-    //   return;
-    // }
-
-    // // Check if description is alphanumeric and contains at least one letter
-    // if (!descriptionRegex.test(formData.description)) {
-    //   toast.error("Description must contain alphanumeric characters");
-    //   return;
-    // }
-
     try {
-      // Create a reference to the file in Firebase storage
       setButtonDisable(true);
       const uploadRef = ref(storage, `News/${file.name}`);
-
-      // Upload the file to Firebase storage
       const snapshot = await uploadBytes(uploadRef, file);
-      console.log("Uploaded a file!");
-
-      // Get the download URL of the uploaded file
       const downloadURL = await getDownloadURL(snapshot.ref);
-      console.log("Download URL:", downloadURL);
-
       const currentTime = serverTimestamp();
-      // Update formData with the image URL
+
       const updatedData = {
         ...formData,
         image: downloadURL,
         time: currentTime,
       };
 
-      // Add the updated data to Firestore
-      const docRef = await addDoc(collection(db, "News"), updatedData);
+      await addDoc(collection(db, "News"), updatedData);
       toast.success("News posted successfully! 🎉");
       setFormData(initialState);
       setFile(null);
@@ -110,81 +95,78 @@ function AddNews() {
 
   return (
     <AdminLayout>
-      <>
-        <div className="addNews">
-          <div className="input-group">
-            <label htmlFor="heading">Heading</label>
-            <textarea
-              name="heading"
-              id="heading"
-              value={formData.heading}
-              onChange={handlechange}
-              cols="2"
-              rows="2"
-              required
-            ></textarea>
-          </div>
-          <div className="input-group">
-            <label htmlFor="description">Description</label>
-            <textarea
-              name="description"
-              id="description"
-              value={formData.description}
-              onChange={handlechange}
-              cols="15"
-              rows="15"
-              required
-            ></textarea>
-          </div>
-          <div className="threeInput-group">
-            <div className="input-group">
-              <label htmlFor="image">Image</label>
-              <input
-                type="file"
-                onChange={handleFileChange}
-                ref={inputFiledRef}
-              />
-            </div>
-            <div className="input-group">
-              <label htmlFor="category">Category</label>
-              <select
-                name="category"
-                id="category"
-                value={formData.category}
-                onChange={handlechange}
-                required
-              >
-                <option value="technology">Technology</option>
-                <option value="sport">Sport</option>
-                <option value="politics">Politics</option>
-                <option value="education">Education</option>
-                <option value="entertainment">Entertainment</option>
-              </select>
-            </div>
-            <div className="input-group">
-              <label htmlFor="isBreaking">Is Breaking</label>
-              <select
-                name="isBreaking"
-                id="isBreaking"
-                value={formData.isBreaking}
-                onChange={handlechange}
-              >
-                <option value="true">Yes</option>
-                <option value="false">No</option>
-              </select>
-            </div>
-
-            <div className="input-group">
-              <label htmlFor="author">Author Name</label>
-              <input type="text" readOnly value={displayName}></input>
-            </div>
-          </div>
-
-          <button onClick={handlePostNews} disabled={buttonDisable}>
-            {buttonDisable ? "Posting" : "Post News"}
-          </button>
+      <div className="addNews">
+        <div className="input-group">
+          <label htmlFor="heading">Heading</label>
+          <textarea
+            name="heading"
+            id="heading"
+            value={formData.heading}
+            onChange={handlechange}
+            cols="2"
+            rows="2"
+            required
+          ></textarea>
         </div>
-      </>
+        <div className="input-group">
+          <label htmlFor="description">Description</label>
+          <textarea
+            name="description"
+            id="description"
+            value={formData.description}
+            onChange={handlechange}
+            cols="15"
+            rows="15"
+            required
+          ></textarea>
+        </div>
+        <div className="threeInput-group">
+          <div className="input-group">
+            <label htmlFor="image">Image</label>
+            <input
+              type="file"
+              accept="image/jpeg, image/png"
+              onChange={handleFileChange}
+              ref={inputFiledRef}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="category">Category</label>
+            <select
+              name="category"
+              id="category"
+              value={formData.category}
+              onChange={handlechange}
+              required
+            >
+              <option value="technology">Technology</option>
+              <option value="sport">Sport</option>
+              <option value="politics">Politics</option>
+              <option value="education">Education</option>
+              <option value="entertainment">Entertainment</option>
+            </select>
+          </div>
+          <div className="input-group">
+            <label htmlFor="isBreaking">Is Breaking</label>
+            <select
+              name="isBreaking"
+              id="isBreaking"
+              value={formData.isBreaking}
+              onChange={handlechange}
+            >
+              <option value="true">Yes</option>
+              <option value="false">No</option>
+            </select>
+          </div>
+          <div className="input-group">
+            <label htmlFor="author">Author Name</label>
+            <input type="text" readOnly value={displayName}></input>
+          </div>
+        </div>
+        <button onClick={handlePostNews} disabled={buttonDisable}>
+          {buttonDisable ? "Posting" : "Post News"}
+        </button>
+      </div>
     </AdminLayout>
   );
 }
